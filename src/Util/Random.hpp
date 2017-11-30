3bc98b398d768667bd1fc631b45678a3c3c839b4
Jiří Šebele <sindriava@gmail.com>
2017-12-01 00:45:33 +0100

    Fix a typo

--- src/Util/Random.hpp
+++ src/Util/Random.hpp
@@ -5,7 +5,7 @@
 
 static float randomFloat() {
   static thread_local std::default_random_engine generator;
-  static thread_local std::uniform_real_distribution<float> distribution(0.0f, 1.0);
+  static thread_local std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
   return distribution(generator);
 }
 
