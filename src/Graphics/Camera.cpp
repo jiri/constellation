dd3074f4b2360a22e8d4c77bf3de30cff77f16ce
Jiří Šebele <sindriava@gmail.com>
2017-12-25 02:23:06 +0100

    Change camera aspect ratio dynamically

--- src/Graphics/Camera.cpp
+++ src/Graphics/Camera.cpp
@@ -2,6 +2,8 @@
 
 #include <glm/gtx/transform.hpp>
 
+#include <SDL2/SDL.h>
+
 GCamera::GCamera(std::string name, const glm::vec3 &p, const glm::vec3 &f)
   : name { std::move(name) }
   , position { p }
@@ -9,6 +11,11 @@ GCamera::GCamera(std::string name, const glm::vec3 &p, const glm::vec3 &f)
 { }
 
 glm::mat4 GCamera::matrix() const {
-  return glm::perspective(glm::radians(75.0f), 4.0f / 3.0f, 0.01f, 100.0f)
+  SDL_Window* window = SDL_GL_GetCurrentWindow();
+  int w = 0;
+  int h = 0;
+  SDL_GetWindowSize(window, &w, &h);
+
+  return glm::perspective(glm::radians(75.0f), float(w) / float(h), 0.01f, 100.0f)
          * glm::lookAt(position, position + front, up);
 }
